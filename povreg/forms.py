4873fee2e7e382e896cb7000b877d644f4798ff6
pugmaster-general <66092816+pugmaster-general@users.noreply.github.com>
2020-07-11 20:02:51 -0400

    updated functions and icons; added in change password form and capability; added in signup function; updated view/update profile to prevent creating dummy objects; modified sign-up so that authenticated users can't accidentally stumble upon it.

--- povreg/forms.py
+++ povreg/forms.py
@@ -1,9 +1,26 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+group_choices = [
+    ('Officers', 'Officers'),
+    ('Drivers', 'Drivers'),
+]
+
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text="Required. Inform a valid email address.")
+    type = forms.CharField(label="Account Type", widget=forms.RadioSelect(choices=group_choices))
+    # print(type.clean())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
 
 
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['email']
+        fields = ['email', ]
 
